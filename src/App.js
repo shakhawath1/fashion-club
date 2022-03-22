a91fdb231a7e3817ed97191b5c610270213a8131
shakhawath <m.shakhawath18@gmail.com>
2022-03-22 20:53:56 +0600

    project fashion-club

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo" />
+      <h1>Hi</h1>
+
+    </div>
+  );
+}
+
+export default App;
